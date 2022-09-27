c6e063d3c1f53df97dff97cde3d0ac900992b7d2
RaufAK <abdulrauf773@gmail.com>
2022-09-26 23:57:34 -0400

    new files

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Abdulrauf Kadelu on 9/26/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}

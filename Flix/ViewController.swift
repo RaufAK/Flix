3d93628d6dc5fe087285595c91459ff2aac68cfc
RaufAK <abdulrauf773@gmail.com>
2022-09-26 22:37:02 -0400

    New files

---
+++ Flix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Abdulrauf Kadelu on 9/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
